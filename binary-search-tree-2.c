3bb3a4cada42a5cdc662482163f248bd0f2ecd3e
brenda <brenda524@naver.com>
2021-05-13 18:41:04 +0900

    commit

--- binary-search-tree-2.c
+++ binary-search-tree-2.c
@@ -47,7 +47,6 @@ int freeBST(Node* head); /* free all memories allocated to the tree */
 
 /* you may add your own defined functions if necessary */
 
-//void printStack();	//Q. 이 함수를 정의해야 하나?
 
 int main()
 {
@@ -100,10 +99,6 @@ int main()
 		case 'l': case 'L':
 			levelOrder(head->left);
 			break;
-
-		// case 'p': case 'P':
-		// 	printStack();
-		// 	break;
 
 		default:
 			printf("\n       >>>>>   Concentration!!   <<<<<     \n");
@@ -198,13 +193,13 @@ int insert(Node* head, int key)
 /* 노드 삭제 함수 -> 3가지 방식 */
 int deleteNode(Node* head, int key)
 {
-	Node* parent=head;  //삭제할 노드의 부모노드 
+	Node* parent=head;  		//삭제할 노드의 부모노드 
 	Node* deleted=head->left;	//삭제할 노드 
 
+	//삭제노드의 right subtree 원소 중 가장 작은 원소 찾는 포인터 
 	Node* ptr=NULL;
 	Node* ptr_parent=NULL;
 
-	//Q. 삭제할 노드를 어떻게 찾을 건가?
 	while(deleted!=NULL)
 	{
 		//해당 key를 가진 노드를 찾을 때 
@@ -213,18 +208,10 @@ int deleteNode(Node* head, int key)
 			/* leaf node delete*/
 			if(deleted->left==NULL&&deleted->right==NULL)
 			{
-				/* head->left가 leaf node일 때*/
-				if(head->left==deleted)
-				{
-					parent->left=NULL;
-					free(deleted);
-					return 0;
-				}
-				/* 그 이외의 노드가 leaf node일 때*/
-				if(key<parent->key)	
-					parent->left=NULL;
-				else if(key>parent->key)
+				if(key>parent->key)
 					parent->right=NULL;
+				else
+					parent->left=NULL;
 				free(deleted);
 				return 0;
 			}
@@ -249,32 +236,10 @@ int deleteNode(Node* head, int key)
 				/* deleted->right->left==NULL일 때(deleted의 right tree의 가장 작은 원소가 deleted->right일 때) */
 				if(deleted->right==ptr)
 				{
-					// if(head->left==deleted)
-					// {
-					// 	parent->left=ptr;
-					// 	ptr->left=deleted->left;
-					// 	free(deleted);
-					// 	return 0;
-					// }
-
-					// //그 이외일 때
-					// if(key<parent->key)
-					// 	parent->left=ptr;
-					// else if(key>parent->key)
-					// 	parent->right=ptr;
-
-					// ptr->left=deleted->left;
-					// free(deleted);
-					// return 0;
-
 					if(key>parent->key)
-					{
 						parent->right=ptr;
-					}	
 					else
-					{
 						parent->left=ptr;
-					}
 					ptr->left=deleted->left;
 					free(deleted);
 					return 0;
@@ -283,42 +248,11 @@ int deleteNode(Node* head, int key)
 				/* deleted->right->left!=NULL일 때(deleted의 right tree의 가장 작은 원소가 deleted->right가 아닐때) */
 				else
 				{
-					// if(head->left==deleted)
-					// {
-					// 	ptr_parent->left=ptr->right;
-					// 	parent->left=ptr;
-					// 	ptr->right=deleted->right;
-					// 	ptr->left=deleted->left;
-					// 	free(deleted);
-					// 	return 0;
-					// }
-
-					// //그 이외의 node가 제거대상일 때
-					// if(key<parent->key)
-					// {
-					// 	ptr_parent->left=ptr->right;
-					// 	parent->left=ptr;
-					// 	ptr->right=deleted->right;
-					// 	ptr->left=deleted->left;
-					// }
-					// else if(key>parent->key)
-					// {
-					// 	ptr_parent->left=ptr->right;
-					// 	parent->right=ptr;
-					// 	ptr->right=deleted->right;
-					// 	ptr->left=deleted->left;
-					// }
-					// free(deleted);
-					// return 0;
 					ptr_parent->left=ptr->right;
 					if(key>parent->key)
-					{
 						parent->right=ptr;
-					}
 					else
-					{
 						parent->left=ptr;
-					}
 					ptr->right=deleted->right;
 					ptr->left=deleted->left;
 					free(deleted);
@@ -328,34 +262,20 @@ int deleteNode(Node* head, int key)
 
 			/* non-leaf node which has one child delete */
 			else
-			{
-				/* head->left가 one child를 갖는 노드일 때*/
-				if(head->left==deleted)
-				{
-					if(deleted->left!=NULL)
-						parent->left=deleted->left;
-					else if(deleted->right!=NULL)
-						parent->left=deleted->right;
-					free(deleted);
-					return 0;
-				}
-
-				//deleted의 subtree는 left or right 중 어디에 위치하는지 알아야 함
-				if(key<parent->key)
-				{
-					if(deleted->left!=NULL)
-						parent->left=deleted->left;
-				
-					else if(deleted->right!=NULL)
-						parent->left=deleted->right;
-				}	
-				else if(key>parent->key)
-				{
+			{			
+				if(key>parent->key){
 					if(deleted->left!=NULL)
 						parent->right=deleted->left;
 				
 					else if(deleted->right!=NULL)
 						parent->right=deleted->right;
+				}
+				else{
+					if(deleted->left!=NULL)
+						parent->left=deleted->left;
+				
+					else if(deleted->right!=NULL)
+						parent->left=deleted->right;					
 				}
 				free(deleted);
 				return 0;
@@ -390,7 +310,7 @@ void freeNode(Node* ptr)
 int freeBST(Node* head)
 {
 	//tree가 존재하지 않을 때 -> head만 메모리 해제시키면 됨
-	if(head->left == head)
+	if(head->left == head)		
 	{
 		free(head);
 		return 1;
@@ -421,17 +341,22 @@ Node* pop()
 /* 스택에 원소 삽입하는 함수 */
 void push(Node* aNode)
 {
+	/* stack이 Full인지 검사 */
 	if(top<MAX_STACK_SIZE-1)	//top>=MAX_STACK_SIZE-1이면, stack이 가득찬 상태로 원소를 삽입할 수 없음
 		stack[++top]=aNode;		//top을 1증가시킨 위치에 원소를 삽입한다
 }
 
 
-/**
- * textbook: p 224s
- */
-/* 비순환 중위순회방식 -> stack 이용*/
+/* 순차적 중위순회방식 -> stack 이용*/
 void iterativeInorder(Node* node)   //LVR 방식 
 {
+	/* 공백 트리인지 검사 */
+	if(!node)	
+	{
+		printf("There is no node in tree\n");
+		return ;		//return ; :아무것도 반환하지 않는, 단지 함수를 종료시키기 위한 
+	}	
+
 	while(1)
 	{
 		for(;node;node=node->left)	//node가 NULL이기 전까지, node의 left로 이동하며, 스택에 node삽입
@@ -439,7 +364,7 @@ void iterativeInorder(Node* node)   //LVR 방식
 			push(node);	//스택에 삽입
 		}
 		node=pop();		//스택에서 삭제 
-		if(!node)  		//공백스택 (pop()==NULL일 때) -> 반복문 탈출
+		if(!node)  		/* 공백스택인지 검사 (pop()==NULL일 때) -> 반복문 탈출 */
 			break;
 		printf(" [%d] ",node->key);	
 		node=node->right;	//right subtree로 이동
@@ -451,8 +376,8 @@ void iterativeInorder(Node* node)   //LVR 방식
 /* queue에서 원소 삭제하는 함수 */
 Node* deQueue()
 {
-	//queue가 empty일 때(rear과 front 동일) -> 삭제할 원소 없으므로 에러 발생 
-	if(rear==front)
+	/* queue가 empty인지 검사 */
+	if(rear==front)		//queue가 empty일 때(rear과 front 동일) -> 삭제할 원소 없음
 		return NULL;	//NULL 반환
 	else
 		return queue[(++front)%MAX_QUEUE_SIZE];	//front를 front+1로 재설정하고, 변경된 front에 위치한 큐의 원소 반환
@@ -462,25 +387,22 @@ Node* deQueue()
 /* queue에서 원소 삽입하는 함수 */
 void enQueue(Node* aNode)
 {
-	//rear+1과 front가 동일하지 않고(queue가 full인 상태x) -> 원소를 queue에 삽입할 수 있음
-	if((rear+1)%MAX_QUEUE_SIZE!=front)	
+	/* queue가 full인지 검사 */
+	if((rear+1)%MAX_QUEUE_SIZE!=front)			//rear+1과 front 동일(queue가 full인 상태) -> ueue에 삽입할 수 없음
 		queue[(++rear)%MAX_QUEUE_SIZE]=aNode;	//rear를 rear+1로 재설정하고, 변경된 rear위치에 aNode 삽입
-	else
-		return ;
 }
 
 
-/**
- * textbook: p 225
- */
 /* 레벨순서 순회(non-recursive) -> circular queue이용 */
 void levelOrder(Node* ptr)
 {
-	if(!ptr)	//공백 트리 
+	/* 공백 트리인지 검사 */
+	if(!ptr)	
 	{
 		printf("There is no node in tree\n");
-		return ;		//QQ. 이렇게 return ;으로 표시하면, 이 함수를 반환하긴 하는거야? 이 함수에서 탈출하는거야?
+		return ;		//return ; :아무것도 반환하지 않는, 단지 함수를 종료시키기 위한 
 	}	
+
 	enQueue(ptr);	//tree의 첫번째 노드 삽입
 	while(1)
 	{
@@ -488,9 +410,9 @@ void levelOrder(Node* ptr)
 		if(ptr)
 		{	
 			printf(" [%d] ",ptr->key);
-			if(ptr->left)				//ptr의 왼쪽노드가 NULL이 아닐때, queue에 원소 삽입
+			if(ptr->left)				//ptr의 왼쪽노드가 존재할 때, queue에 원소 삽입
 				enQueue(ptr->left);
-			if(ptr->right)				//ptr의 오른쪽노드가 NULL이 아닐때, queue에 원소 삽입
+			if(ptr->right)				//ptr의 오른쪽노드가 존재할 때, queue에 원소 삽입
 				enQueue(ptr->right);
 		}
 		//ptr=NULL일 때, 반복문 탈출 
